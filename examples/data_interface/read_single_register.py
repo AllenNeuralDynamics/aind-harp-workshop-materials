69b2e5e2804c4dbdb3be0a69b89f35091f93549e
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-04-18 11:07:40 -0700

    Read both files

--- examples/data_interface/read_single_register.py
+++ examples/data_interface/read_single_register.py
@@ -1,6 +1,10 @@
 import harp
 
 if __name__ == "__main__":
-    file = "./data/Register_DigitalInputstate.bin"
+    file = "./data/Register_DigitalInputState.bin"
+    data = harp.read(file)
+    print(data)
+
+    file = "./data/Register_AnalogData.bin"
     data = harp.read(file)
     print(data)
