f6e6902ff7aba8cc67bd888e7fa452db7d2da4b8
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-04-18 11:10:20 -0700

    Fix example with device schema

--- examples/data_interface/read_single_register_with_interface.py
+++ examples/data_interface/read_single_register_with_interface.py
@@ -1,7 +1,8 @@
 import harp
 
 if __name__ == "__main__":
-    device = harp.create_reader("device.yml")
+    device = harp.create_reader("./data/device.yml")
+    print(dir(device))
     file = "./data/Register_DigitalInputState.bin"
-    data = device.DigitalInputState(file)
+    data = device.DigitalInputState.read(file)
     print(data)
